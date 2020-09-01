704547e56376446a325c00356f454927f2e2f9ae
dms-yondy <price1858@gmail.com>
2020-09-01 16:26:41 +0100

    changed don't learn in app

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Learn React
         </a>
       </header>
     </div>
